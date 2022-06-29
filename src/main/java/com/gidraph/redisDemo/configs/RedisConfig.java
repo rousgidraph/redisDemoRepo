7588e01c3d93487d162f58eef3a420dc5c22257a
Gidraph Muriuki <gidraph.kinyua@cellulant.io>
2022-06-29 09:49:53 +0300

    Working, current error message Factory was not initialized through afterPropertiesSet() :wrench:

--- src/main/java/com/gidraph/redisDemo/configs/RedisConfig.java
+++ src/main/java/com/gidraph/redisDemo/configs/RedisConfig.java
@@ -4,6 +4,7 @@ import com.gidraph.redisDemo.domains.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -14,30 +15,25 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Slf4j
 public class RedisConfig {
 
-    public JedisConnectionFactory jedisConnectionFactory() {
-        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
 
-        jedisConFactory.setHostName("localhost");
-        jedisConFactory.setPort(6379);
-        jedisConFactory.setPassword("");
+    @Primary
+    public JedisConnectionFactory jedisConnectionFactory(){
+        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();  
+             
         log.info("first");
         return jedisConFactory;
     }
 
+
     @Bean
+    @Primary
     public RedisOperations<String, Message> redisOperations() {
-        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
-
-        jedisConFactory.setHostName("localhost");
-        jedisConFactory.setPort(6379);
-        jedisConFactory.setPassword("");
-        log.info("first");
-
+       
         Jackson2JsonRedisSerializer<Message> serializer = new Jackson2JsonRedisSerializer<>(Message.class);
 
         log.info("second");
         RedisTemplate<String, Message> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConFactory);
+        template.setConnectionFactory(jedisConnectionFactory());
         template.setDefaultSerializer(serializer);
         template.setKeySerializer(new StringRedisSerializer());
 
